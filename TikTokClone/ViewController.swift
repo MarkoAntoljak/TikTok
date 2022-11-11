1715188bdafaad840361a020a547e255c556edba
Marko Antoljak <marko.antoljak6@gmail.com>
2022-11-11 09:07:37 -0800

    Initial Commit

---
+++ TikTokClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokClone
+//
+//  Created by Marko Antoljak on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
